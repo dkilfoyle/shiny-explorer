48ea374c4d6d53642af038b88007570dda44efda
Dean Kilfoyle <dkilfoyle@xtra.co.nz>
2013-08-05 22:35:17 +1200

    Add regression to numeric2 and diagnostic plots

--- R/dkgraph.r
+++ R/dkgraph.r
@@ -750,3 +750,29 @@ if(table){
   if(returns) return(p)
 }
 }
+
+# for arranging multiple ggplots in 1
+# taken from http://www.stat.harvard.edu/Research/r/test0suppl.R
+
+vp.layout <- function(x, y) viewport(layout.pos.row=x, layout.pos.col=y) 
+
+ggplotarrange <- function(..., nrow=NULL, ncol=NULL, as.table=FALSE) {  
+  dots <- list(...)  
+  n <- length(dots)  
+  if(is.null(nrow) & is.null(ncol)) { nrow = floor(n/2) ; ncol = ceiling(n/nrow)}  
+  if(is.null(nrow)) { nrow = ceiling(n/ncol)}  
+  if(is.null(ncol)) { ncol = ceiling(n/nrow)}         
+  grid.newpage() 
+  pushViewport(viewport(layout=grid.layout(nrow,ncol) ) )  
+  ii.p <- 1  
+  for(ii.row in seq(1, nrow)){  
+    ii.table.row <- ii.row   
+    if(as.table) {ii.table.row <- nrow - ii.table.row + 1}   
+    for(ii.col in seq(1, ncol)){   
+      ii.table <- ii.p   
+      if(ii.p > n) break  
+      print(dots[[ii.table]], vp=vp.layout(ii.table.row, ii.col))   
+      ii.p <- ii.p + 1   
+    } 
+  } 
+}
